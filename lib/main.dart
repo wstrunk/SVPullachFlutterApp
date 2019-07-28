83dfc33299451d924318630f2bb1cc11c2a2c5dc
Wolfgang Strunk <Wolfgang.Strunk@iteratec.de>
2019-07-28 23:39:06 +0200

    new start page from html

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,107 @@
-// This sample shows adding an action to an [AppBar] that opens a shopping cart.
-
 import 'package:flutter/material.dart';
+
+//import 'package:flutter_app/src/Widgets/flutter_native_web.dart';
+import 'package:flutter_native_web/flutter_native_web.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_app/src/Widgets/SvpScaffold.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  WebController webController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Code Sample for material.AppBar.actions',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+
+    FlutterNativeWeb flutterWebView = new FlutterNativeWeb(
+      onWebCreated: onWebCreated,
+      gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
+        Factory<OneSequenceGestureRecognizer>(
+              () => TapGestureRecognizer(),
+        ),
+      ].toSet(),
+    );
+
+    return new MaterialApp(
+      home: new SvpScaffold(
+          body: new SingleChildScrollView(
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  child: flutterWebView,
+                  height: 600.0,
+                  width: 500.0,
+                ),
+              ],
+            ),
+          )
       ),
-      home: MyStatelessWidget(),
     );
   }
-}
 
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+  String html = '<body>' +
+  '<h1>Sportverein Pullach i. Isartal e.V.</h1>' +
+  '<h4>Abteilung Handball im Deutschen Handballbund (DHB)</h4>' +
+  '<p>Vereinsregister (Hauptverein): Amtsgericht München, VR 8796</p>' +
+  '<p>Gistlstraße. 2<br>' +
+  'D-82049 Pullach i.Isartal</p>' +
+  '<p>Vertretungsberechtigt für den SV Pullach Abt. Handball ist der Vorsitzende<br>' +
+  'Dirk Trautmann<br>' +
+  'Kastanienallee 6, 82049 Pullach<br>' +
+  'E-Mail: <a href="mailto:vorstand@svpullach-handball.de">vorstand@svpullach-handball.de</a></p>' +
+  '<p>sowie dessen Stellvertreter</p>' +
+  '<p style="font-weight: 400;">Benedikt Thelen<br>' +
+  'Wolfratshauser Str. 76, 82049 Pullach<br>' +
+  'E-Mail:<span>&nbsp;</span><a href="mailto:benedikt.thelen@svpullach-handball.de">benedikt.thelen@svpullach-handball.de</a></p>' +
 
-  @override
-  Widget build(BuildContext context) {
+  '<h1>Sponsoren</h1>' +
+  '<table>' +
+  '<tbody>' +
+  '<tr>' +
+  '<td><a href="https://www.edeka.de/eh/sdbayern/edeka-hltkemeyer-theresienhhe-12/index.jsp" target="_blank" rel="noopener"><img style="display: block; margin-left: auto; margin-right: auto;" src="http://www.svpullach-handball.de/wp-content/uploads/2019/07/Logo-Edeka_Theresie-01.png" width="350"  alt="" /></a></td>' +
+  '</tr>' +
+  '<tr>' +
+  '<td><a href="https://www.allfinanz.ag/Oliver.Muehldorfer/index.html" target="_blank" rel="noopener"><img src="http://www.svpullach-handball.de/wp-content/uploads/2019/07/oliver_muehldorfer_logo.png" alt="" width="350" height="175" /></a></td>' +
+  '</tr>' +
+  '<tr style="text-align: center;">' +
+  '<td><a href="www.weplayhandball.de" target="_blank" rel="noopener"><img style="display: block; margin-left: auto; margin-right: auto;" src="http://www.svpullach-handball.de/wp-content/uploads/2015/01/WPH_Logo.jpg" alt="" /></a></td>' +
+  '</tbody>' +
+  '</table>' +
+      '</body>'  ;
 
-    return SvpScaffold(
-      body: Center(child: Text('My Page!')),
+
+  void onWebCreated(webController) {
+    this.webController = webController;
+    this.webController.loadData(html);
+    this.webController.onPageStarted.listen((url) =>
+        print("Loading $url")
+    );
+    this.webController.onPageFinished.listen((url) =>
+        print("Finished loading $url")
     );
   }
-}
 
+//  void onWebCreated(webController) {
+//    this.webController = webController;
+//    this.webController.loadUrl("https://www.amazon.com");
+//    this.webController.onPageStarted.listen((url) =>
+//        print("Loading $url")
+//    );
+//    this.webController.onPageFinished.listen((url) =>
+//        print("Finished loading $url")
+//    );
+//  }
+}
