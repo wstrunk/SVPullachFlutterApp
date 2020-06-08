4e0b684cde1c00cb1cdd27b427eecfd55b543544
Wolfgang Strunk <Wolfgang.Strunk@iteratec.de>
2020-06-08 12:29:38 +0200

    merge master and remove about screen

--- lib/main.dart
+++ lib/main.dart
@@ -38,10 +38,6 @@ class MyApp extends StatelessWidget {
 
   WebViewController _controller;
 
-  @override
-  void initState() {
-    super.initState();
-  }
   MyApp({Key key, @required UserRepository userRepository})
       : assert(userRepository != null),
         _userRepository = userRepository,
@@ -65,39 +61,7 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  String html = '<body>' +
-  '<h1>Sportverein Pullach i. Isartal e.V.</h1>' +
-  '<h4>Abteilung Handball im Deutschen Handballbund (DHB)</h4>' +
-  '<p>Vereinsregister (Hauptverein): Amtsgericht München, VR 8796</p>' +
-  '<p>Gistlstraße. 2, D-82049 Pullach i.Isartal</p>' +
-  '<p>Vertretungsberechtigt für den SV Pullach Abt. Handball ist der Vorsitzende<br>' +
-  'Dirk Trautmann, Kastanienallee 6, 82049 Pullach<br>' +
-  'E-Mail: <a href="mailto:vorstand@svpullach-handball.de">vorstand@svpullach-handball.de</a></p>' +
-  '<p>sowie dessen Stellvertreter' +
-  'Benedikt Thelen, Wolfratshauser Str. 76, 82049 Pullach<br>' +
-  'E-Mail:<span>&nbsp;</span><a href="mailto:benedikt.thelen@svpullach-handball.de">benedikt.thelen@svpullach-handball.de</a></p>' +
-  '<h1>Sponsoren</h1>' +
-  '<table>' +
-  '<tbody>' +
-  '<tr>' +
-  '<td><a href="https://www.edeka.de/eh/sdbayern/edeka-hltkemeyer-theresienhhe-12/index.jsp" target="_blank" rel="noopener"><img style="display: block; margin-left: auto; margin-right: auto;" src="http://www.svpullach-handball.de/wp-content/uploads/2019/07/Logo-Edeka_Theresie-01.png" width="350"  alt="" /></a></td>' +
-  '</tr>' +
-  '<tr>' +
-  '<td><a href="https://www.allfinanz.ag/Oliver.Muehldorfer/index.html" target="_blank" rel="noopener"><img src="http://www.svpullach-handball.de/wp-content/uploads/2019/07/oliver_muehldorfer_logo.png" alt="" width="350" height="175" /></a></td>' +
-  '</tr>' +
-  '<tr style="text-align: center;">' +
-  '<td><a href="www.weplayhandball.de" target="_blank" rel="noopener"><img style="display: block; margin-left: auto; margin-right: auto;" src="http://www.svpullach-handball.de/wp-content/uploads/2015/01/WPH_Logo.jpg" alt="" /></a></td>' +
-  '</tbody>' +
-  '</table>' +
-      '</body>'  ;
 
 
-  _loadHtmlFromString() async {
-    _controller.loadUrl( Uri.dataFromString(
-        html,
-        mimeType: 'text/html',
-        encoding: Encoding.getByName('utf-8')
-    ).toString());
-  }
 
 }
